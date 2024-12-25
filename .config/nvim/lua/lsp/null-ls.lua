a1945971484f3e0bd9ffe17c21f24e14b1b93cc2
MasterMind7777777 <go.go.nest.nest@gmail.com>
2024-12-25 20:20:36 -0300

    nvim beta version fixes before proper realse.

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -1,18 +1,38 @@
 local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- Define a custom source for rustfmt
+local rustfmt = {
+	name = "rustfmt",
+	method = null_ls.methods.FORMATTING,
+	filetypes = { "rust" },
+	generator = helpers.formatter_factory({
+		command = "rustfmt",
+		args = { "--emit=stdout", "--edition=2021" },
+		to_stdin = true,
+	}),
+}
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettier, -- Prettier for JS/TS
-		null_ls.builtins.formatting.stylua, -- Stylua for Lua
-		null_ls.builtins.formatting.dxfmt, -- dxfmt for Rust
+		-- Stylua for Lua
+		null_ls.builtins.formatting.stylua,
+
+		null_ls.builtins.formatting.prettier,
+
+		rustfmt,
 	},
 	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			-- Create an autocommand to format on save
+		if client.server_capabilities.documentFormattingProvider then
+			-- Clear existing autocommands
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			-- Create the autocommand
 			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					vim.lsp.buf.format({ async = false })
+					vim.lsp.buf.format({ bufnr = bufnr, async = false })
 				end,
 			})
 		end
--- bash/hyperland/toggle_layout.sh
+++ bash/hyperland/toggle_layout.sh
@@ -1,8 +1,43 @@
-#!/bin/bash
-current_layout=$(setxkbmap -query | awk '/layout/ {print $2}')
-if [ "$current_layout" = "us" ]; then
-    setxkbmap -layout ru
-else
-    setxkbmap -layout us
-fi
+#!/usr/bin/env bash
 
+# Query the current keyboard layout for the main keyboard
+current_layout=$(
+  hyprctl devices | awk '
+    # Whenever a new Keyboard at ... line appears, clear any previously stored layout
+    /Keyboard at/ {
+      layout = ""
+    }
+
+    # When we see a "rules:" line, look for the token "l" and take the next field
+    /rules:/ {
+      for (i = 1; i <= NF; i++) {
+        if ($i == "l") {
+          layout = $(i + 1)
+        }
+      }
+    }
+
+    # If we see "main: yes", that means the current device is the main keyboard
+    /main: yes/ {
+      if (layout != "") {
+        print layout
+        exit
+      }
+    }
+  ' | tr -d '",'
+)
+
+# Toggle between layouts
+if [ "$current_layout" = "us" ]; then
+    hyprctl keyword input:kb_layout ru
+else
+    hyprctl keyword input:kb_layout us
+fi
+# #!/bin/bash
+# current_layout=$(setxkbmap -query | awk '/layout/ {print $2}')
+# if [ "$current_layout" = "us" ]; then
+#     setxkbmap -layout ru
+# else
+#     setxkbmap -layout us
+# fi
+#
