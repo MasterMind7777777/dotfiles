68966270259fba34391985551b7bc608a18aa6dd
MasterMind7777777 <go.go.nest.nest@gmail.com>
2025-02-04 09:50:44 -0300

    fix oil config

--- .config/nvim/lua/plugins_conf/oil.lua
+++ .config/nvim/lua/plugins_conf/oil.lua
@@ -1,8 +1,11 @@
 local M = {}
 
 M.setup = function()
-	-- Configure Oil.nvim settings
-	vim.g.oil_setup = {
+	require("oil").setup({
+		default_file_explorer = true,
+		columns = {
+			"icon",
+		},
 		buf_options = {
 			buflisted = false,
 			bufhidden = "hide",
@@ -15,7 +18,7 @@ M.setup = function()
 			spell = false,
 			list = false,
 			conceallevel = 3,
-			concealcursor = "nvc",
+			concealcursor = "nvic",
 		},
 		delete_to_trash = false,
 		skip_confirm_for_simple_edits = false,
@@ -24,7 +27,7 @@ M.setup = function()
 		lsp_file_methods = {
 			enabled = true,
 			timeout_ms = 1000,
-			autosave_changes = true,
+			autosave_changes = true, -- changed
 		},
 		constrain_cursor = "editable",
 		watch_for_changes = false,
@@ -32,11 +35,111 @@ M.setup = function()
 			["g?"] = "actions.show_help",
 			["<CR>"] = "actions.select",
 			["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+			["<C-h>"] = {
+				"actions.select",
+				opts = { horizontal = true },
+				desc = "Open the entry in a horizontal split",
+			},
+			["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["`"] = "actions.cd",
+			["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+			["gs"] = "actions.change_sort",
+			["gx"] = "actions.open_external",
+			["g."] = "actions.toggle_hidden",
+			["g\\"] = "actions.toggle_trash",
 		},
-	}
-
-	-- Now, just call setup without arguments
-	require("oil").setup()
+		use_default_keymaps = true,
+		view_options = {
+			show_hidden = true,
+			is_hidden_file = function(name, bufnr)
+				return name:match("^%.") ~= nil
+			end,
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+			natural_order = "fast",
+			case_insensitive = false,
+			sort = {
+				{ "type", "asc" },
+				{ "name", "asc" },
+			},
+		},
+		extra_scp_args = {},
+		git = {
+			add = function(path)
+				return false
+			end,
+			mv = function(src_path, dest_path)
+				return false
+			end,
+			rm = function(path)
+				return false
+			end,
+		},
+		float = {
+			padding = 2,
+			max_width = 0,
+			max_height = 0,
+			border = "rounded",
+			win_options = {
+				winblend = 10,
+				winhl = "Normal:Normal,Float:Float",
+			},
+			get_win_title = nil,
+			preview_split = "auto",
+			override = function(conf)
+				return conf
+			end,
+		},
+		preview_win = {
+			update_on_cursor_moved = true,
+			preview_method = "fast_scratch",
+			disable_preview = function(filename)
+				return false
+			end,
+			win_options = {
+				winblend = 10,
+				winhl = "Normal:Normal,Float:Float",
+			},
+		},
+		confirmation = {
+			max_width = 0.9,
+			min_width = { 40, 0.4 },
+			width = nil,
+			max_height = 0.9,
+			min_height = { 5, 0.1 },
+			height = nil,
+			border = "rounded",
+			win_options = {
+				winblend = 10,
+				winhl = "Normal:Normal,Float:Float",
+			},
+		},
+		progress = {
+			max_width = 0.9,
+			min_width = { 40, 0.4 },
+			width = nil,
+			max_height = { 10, 0.9 },
+			min_height = { 5, 0.1 },
+			height = nil,
+			border = "rounded",
+			minimized_border = "none",
+			win_options = {
+				winblend = 10,
+				winhl = "Normal:Normal,Float:Float",
+			},
+		},
+		ssh = {
+			border = "rounded",
+		},
+		keymaps_help = {
+			border = "rounded",
+		},
+	})
 	vim.cmd([[
 " Oil highlights with Gruvbox color palette
 
