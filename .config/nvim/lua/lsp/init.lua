c41fab1f36618ec9ef85291cb7316b24d332e58f
MasterMind7777777 <go.go.nest.nest@gmail.com>
2025-02-08 12:15:05 -0300

    update hypr and nvim minor

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,7 +1,7 @@
 -- Mason setup (LSP installer)
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "ts_ls", "lua_ls", "rust_analyzer" },
+	ensure_installed = { "ts_ls", "lua_ls" },
 })
 
 -- Load specific LSP configurations
